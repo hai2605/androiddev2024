cb1cfeae0a25e23f365ee1aa8a52b493cb6471b4
hai2605 <nguyenhaibr5@gmail.com>
2024-08-23 15:04:16 +0700

    Practical 1 and 2

---
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -0,0 +1,56 @@
+package vn.edu.usth.weather;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class WeatherActivity extends AppCompatActivity {
+    private static final String Tag = "WeatherActivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+        Log.i(Tag,"Create");
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Log.i(Tag,"Start");
+
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Log.i(Tag,"Resume");
+
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Log.i(Tag,"Pause");
+
+    }
+
+    @Override
+    protected void onStop(){
+        Log.i(Tag,"Stop");
+        super.onStop();
+
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.i(Tag,"Destroy");
+
+    }
+}
