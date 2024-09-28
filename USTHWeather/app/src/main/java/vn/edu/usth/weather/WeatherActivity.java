ec7e6d34e529d972518c5a1b5e9e79b92acde78f
hai2605 <nguyenhaibr5@gmail.com>
2024-09-28 15:33:27 +0700

    Pratical work 11

--- USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -1,7 +1,10 @@
 package vn.edu.usth.weather;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,8 +15,14 @@ import androidx.fragment.app.Fragment;
 import androidx.viewpager.widget.ViewPager;
 import com.google.android.material.tabs.TabLayout;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
 public class WeatherActivity extends AppCompatActivity {
     private static final String Tag = "WeatherActivity";
+    private MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +31,6 @@ public class WeatherActivity extends AppCompatActivity {
 
         HomeFragmentPagerAdapter adapter = new HomeFragmentPagerAdapter(getSupportFragmentManager());
 
-
         ViewPager pager = (ViewPager) findViewById(R.id.pager);
         pager.setOffscreenPageLimit(3);
         pager.setAdapter(adapter);
@@ -30,6 +38,35 @@ public class WeatherActivity extends AppCompatActivity {
         tabLayout.setupWithViewPager(pager);
 
         Log.i(Tag, "Create");
+    }
+
+    private void extractAndPlayMusic() {
+        File musicFile = new File(Environment.getExternalStorageDirectory(), "example.mp3");
+        if (!musicFile.exists()) {
+            try {
+                InputStream is = getResources().openRawResource(R.raw.example);  // Replace 'sample' with your MP3 file name
+                FileOutputStream fos = new FileOutputStream(musicFile);
+                byte[] buffer = new byte[1024];
+                int length;
+                while ((length = is.read(buffer)) > 0) {
+                    fos.write(buffer, 0, length);
+                }
+                fos.close();
+                is.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        // Play the extracted MP3 file
+        mediaPlayer = new MediaPlayer();
+        try {
+            mediaPlayer.setDataSource(musicFile.getAbsolutePath());
+            mediaPlayer.prepare();
+            mediaPlayer.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -63,6 +100,10 @@ public class WeatherActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        if (mediaPlayer != null) {
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
         Log.i(Tag, "Destroy");
 
     }
